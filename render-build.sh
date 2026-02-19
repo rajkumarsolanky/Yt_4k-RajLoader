cee263332e0ffa4b656da0c8ec55d5943f802275
Raj Kumar <134475846+rajkumarsolanky@users.noreply.github.com>
2026-02-19 11:33:18 +0500

    Remove Python and FFmpeg installation steps
    Removed installation steps for Python dependencies and FFmpeg.

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# 1. Python dependencies install karein
 pip install -r requirements.txt
 
-# 2. FFmpeg install karne ke liye (Download URL ke saath)
 mkdir -p /tmp/ffmpeg
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ --strip-components=1 -C /tmp/ffmpeg
 
-# 3. Path set karein taaki system ise dhoond sake
 export PATH=$PATH:/tmp/ffmpeg
